b3db298b539c8f06321ae875c9134979449e70d2
mtmcduffie <mcduffie.michael@gmail.com>
2016-08-23 14:57:39 -0400

    Testing Webhook.

--- run_UPS.sh
+++ run_UPS.sh
@@ -1,6 +1,6 @@
 docker stop ups
 docker rm ups
 
-docker run --name ups -p 8000:8000 -i -t dbmi/upload-preprocessing-service
+docker run --name ups -p 8000:8000 -i -t dbmi/upload-preprocessing-service:latest
 
 #docker exec -i -t ups /bin/bash
