b49b1f27beb84b26c9f3dd185d92c62ab7d0706f
mtmcduffie <mcduffie.michael@gmail.com>
2016-09-20 14:53:47 -0400

    UDNTECH-633 - Adding vault to id-store-app

--- Dockerfile
+++ Dockerfile
@@ -15,10 +15,6 @@ RUN rm -rf /etc/nginx/sites-available/default
 RUN mkdir /etc/nginx/ssl/
 RUN chmod 710 /etc/nginx/ssl/
 
-COPY gunicorn-nginx-entry.sh /
-
-RUN chmod u+x gunicorn-nginx-entry.sh
-
 RUN mkdir /idstore-app/
 RUN mkdir /idstore-app/static/
 
@@ -27,6 +23,18 @@ WORKDIR /idstore-app/
 RUN  echo "hi" && git clone -b development https://github.com/hms-dbmi/idstore-app.git
 
 RUN pip install -r /idstore-app/idstore-app/requirements.txt
+
+RUN	apt-get -y update && \
+ 	apt-get -y install unzip
+ 	
+RUN mkdir /vault/
+COPY vault_0.6.1_linux_amd64.zip /vault/
+RUN unzip /vault/vault_0.6.1_linux_amd64.zip -d /vault/
+RUN ls -l /vault
+
+COPY gunicorn-nginx-entry.sh /
+
+RUN chmod u+x /gunicorn-nginx-entry.sh
 
 WORKDIR /
 
--- gunicorn-nginx-entry.sh
+++ gunicorn-nginx-entry.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+#vault write -format json auth/app-id/login app_id=$APP_ID user_id=$USER_ID | jq -r .auth.client_token > /root/secretstash.txt
+
+echo $ONETIME_TOKEN
+
+export VAULT_ADDR=https://vault.aws.dbmi.hms.harvard.edu:443 
+export VAULT_SKIP_VERIFY=1
+
+/vault/vault auth $ONETIME_TOKEN
+
+DJANGO_SECRET=$(/vault/vault read -field=value secret/udn/udndev/ups_django_secret)
+UPS_MYSQL_USERNAME_VAULT=$(/vault/vault read -field=value secret/udn/udndev/ups_mysql_username)
+UPS_MYSQL_PASSWORD_VAULT=$(/vault/vault read -field=value secret/udn/udndev/ups_mysql_password)
+UPS_MYSQL_PORT_VAULT=$(/vault/vault read -field=value secret/udn/udndev/ups_mysql_port)
+UPS_MYSQL_HOSTNAME_VAULT=$(/vault/vault read -field=value secret/udn/udndev/ups_mysql_hostname)
+
+UPS_KEY=$(/vault/vault read -field=value secret/udn/udndev/ups_ssl_key)
+UPS_CERT=$(/vault/vault read -field=value secret/udn/udndev/ups_ssl_cert)
+UPS_CERT_CHAIN=$(/vault/vault read -field=value secret/udn/udndev/ups_ssl_cert_chain)
+
+export SECRET_KEY=$DJANGO_SECRET
+export UPS_MYSQL_USERNAME=$UPS_MYSQL_USERNAME_VAULT
+export UPS_MYSQL_PASSWORD=$UPS_MYSQL_PASSWORD_VAULT
+export UPS_MYSQL_PORT=$UPS_MYSQL_PORT_VAULT
+export UPS_MYSQL_HOSTNAME=$UPS_MYSQL_HOSTNAME_VAULT
+
 echo $UPS_KEY | base64 -d >> /etc/nginx/ssl/UPS_KEY.key
 echo $UPS_CERT | base64 -d >> /etc/nginx/ssl/UPS_CERT.crt
 echo $UPS_CERT_CHAIN | base64 -d >> /etc/nginx/ssl/UPS_CERT.crt
@@ -14,5 +39,3 @@ python manage.py collectstatic --no-input
 /etc/init.d/nginx restart
 
 gunicorn idstore-app.wsgi:application
-
-
